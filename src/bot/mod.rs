07731e7dc05c4eb8b2d56cb05902ff2ab0dcec9a
Jendorski <gabjide@gmail.com>
2025-10-15 17:17:08 +0000

    Updated

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -542,12 +542,10 @@ impl Bot {
                     config.risk_pct,
                     Position::Long,
                 );
-                let should_close = Self::should_close(
-                    self.open_pos.entry_price,
-                    self.pos,
-                    self.open_pos.sl.unwrap_or(in_sl),
-                );
+                let should_close =
+                    Self::should_close(price, self.pos, self.open_pos.sl.unwrap_or(in_sl));
                 warn!("should_close -> {:?}, in_sl -> {:?}", should_close, in_sl);
+
                 if should_close {
                     Self::close_long_position(self, price).await;
 
@@ -582,11 +580,8 @@ impl Bot {
                     config.risk_pct,
                     Position::Long,
                 );
-                let should_close = Self::should_close(
-                    self.open_pos.entry_price,
-                    self.pos,
-                    self.open_pos.sl.unwrap_or(in_sl),
-                );
+                let should_close =
+                    Self::should_close(price, self.pos, self.open_pos.sl.unwrap_or(in_sl));
 
                 if should_close {
                     Self::close_short_position(self, price).await;
