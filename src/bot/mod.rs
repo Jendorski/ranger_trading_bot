a08e2f5c87ad36f20984b3d86add0cfd35417913
Jendorski <gabjide@gmail.com>
2025-10-15 17:06:01 +0000

    Updated

--- src/bot/mod.rs
+++ src/bot/mod.rs
@@ -544,7 +544,7 @@ impl Bot {
                 );
                 let should_close =
                     Self::should_close(price, self.pos, self.open_pos.sl.unwrap_or(in_sl));
-                warn!("should_close -> {:?}", should_close);
+                warn!("should_close -> {:?}, in_sl -> {:?}", should_close, in_sl);
                 if should_close {
                     Self::close_long_position(self, price).await;
 
@@ -582,7 +582,7 @@ impl Bot {
                 //Trigger SL if it's met
                 let should_close =
                     Self::should_close(price, self.pos, self.open_pos.sl.unwrap_or(in_sl));
-                warn!("should_close -> {:?}", should_close);
+                warn!("should_close -> {:?}, in_sl -> {:?}", should_close, in_sl);
                 if should_close {
                     Self::close_short_position(self, price).await;
 
